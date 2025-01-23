b5ae67bba497428f260e02fe0c9eea30db934fc4
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-01-23 22:43:35 +0200

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_1
+{
+    /// <summary>
+    /// Класс "Человек"
+    /// </summary>
+    internal class Person
+    {
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string firstName;
+
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string lastName;
+
+        /// <summary>
+        /// Возраст
+        /// </summary>
+        public int age;
+        
+        /// <summary>
+        /// Метода вывода информации о человеке на консоль
+        /// </summary>
+        public void Output()
+        {
+            Console.WriteLine($"Имя: {firstName}\tФамилия: {lastName}\tВозраст: {age}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ilya = new Person();
+
+            ilya.firstName = "Илья";
+            ilya.lastName = "Шичкин";
+            ilya.age = 17;
+
+            ilya.Output();
+
+            Console.ReadKey();
+        }
+    }
+}
